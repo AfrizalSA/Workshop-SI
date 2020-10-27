083e734a62cda53714460f69bb5222ff06c0fed3
Arifkrn <71825520+Arifkrn@users.noreply.github.com>
2020-10-27 19:03:51 +0700

    belajar php

---
+++ E41192283_ArifKurniawan/belajar_php/sintax_html_php.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo "Belajar PHP" ?></title>
+</head>
+<body>
+    <?php
+    echo "saya sedang belajar PHP<br>";
+    echo "<p>Belajar PHP hingga jadi master</p>";
+    ?>
+    
+</body>
+</html>
---
+++ E41192283_ArifKurniawan/belajar_php/variable.php
@@ -0,0 +1,8 @@
+<?php
+// membuat variabel baru
+$nama_barang = "Minyak Goreng";
+$harga = 15000;
+
+// Menampilkan isi variabel
+echo "Ibu membeli $nama_barang seharga Rp $harga";
+?>
