ee9b8a4141fbce7e3cdb07ea9cbf647371857419
Alga Saputra <68761934+AlgaSaputra@users.noreply.github.com>
2020-10-27 22:25:50 +0700

    Belajar PHP

---
+++ E41192329_AlgaSaputra_FormKasir/belajar_php/konstanta.php
@@ -0,0 +1,13 @@
+<?php //file: belajar-konstanta.php
+
+// membuat konstanta
+define('VERSION', '1.0.0');
+
+const SITE_NAME = "PetaniKode";
+const BASE_URL = "https://www.petanikode.com";
+
+// cetak nilai konstanta
+echo "Site Name: " . SITE_NAME . "<br/>";
+echo "URL: " . BASE_URL . "<br/>";
+echo "Version: " . VERSION . "<br/";
+?>
---
+++ E41192329_AlgaSaputra_FormKasir/belajar_php/print.php
@@ -0,0 +1,5 @@
+<?php
+print "<h2>Belajar PHP dari 0!</h2>";
+print "Hello World!<br>";
+print "Belajar mencetak teks di PHP!";
+?>
