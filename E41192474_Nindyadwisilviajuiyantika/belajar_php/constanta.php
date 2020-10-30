49c98f191aaf7a480aa1429ab6cc0250420f49ae
NindyaDwiSilvia <71866926+NindyaDwiSilvia@users.noreply.github.com>
2020-10-30 07:48:42 +0700

    Belajar_PHP.6

---
+++ E41192474_Nindyadwisilviajuiyantika/belajar_php/constanta.php
@@ -0,0 +1,13 @@
+<?php // file: <belajar-konstanta class="php"></belajar-konstanta>
+
+// membuat konstanta
+define('VERSION', '1.0.0');
+
+const SITE_NAME = "Petanikode";
+const BASE_URL  = "https://www.petanikode.com";
+
+// cetak nilai konstanta
+echo "site name: " . SITE_NAME . "<br/>";
+echo "URL: " . BASE_URL . "<br/>";
+echo "Version: " . VERSION . "<br/>";
+?>
---
+++ E41192474_Nindyadwisilviajuiyantika/belajar_php/sintax_html_php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?php echo "Belajar PHP" ?></title>
+   </head>
+    <body>
+        <?php
+            echo "saya sedang belajar PHP<br>";
+            echo "<p>Belajar PHP hingga jadi master</p>";
+        ?>
+</body>
+</html>
