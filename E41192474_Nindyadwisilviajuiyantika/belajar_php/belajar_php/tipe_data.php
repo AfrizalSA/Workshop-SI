4588404cb0576caa04c4e2dcf3d8659a6ea8edb1
NindyaDwiSilvia <71866926+NindyaDwiSilvia@users.noreply.github.com>
2020-10-30 02:10:53 +0100

    Tugas PHP.6

---
+++ E41192474_Nindyadwisilviajuiyantika/belajar_php/belajar_php/tipe_data.php
@@ -0,0 +1,27 @@
+<?php
+
+// tipe data char (karakter)
+$Jenis_kelamin = 'L';
+
+// tipe data string (teks) 
+$nama_lengkap = "petani kode";
+
+// tipe data integer 
+$umur = 20;
+
+// tipe data float 
+$berat = 48.3;
+
+// tipe data float
+$tinggi = 182.2;
+
+// tipe data boolean
+$menikah = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis kelamin: $Jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "menikah: $menikah";
+?>
