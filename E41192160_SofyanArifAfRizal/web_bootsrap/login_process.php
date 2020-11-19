d25cbeae740d903bc2e8c4a729c82c1977cbcc46
AfrizalSA <71804747+AfrizalSA@users.noreply.github.com>
2020-11-19 17:12:58 +0100

    praktek_blok

--- E41192160_SofyanArifAfRizal/web_bootsrap/login_process.php
+++ E41192160_SofyanArifAfRizal/web_bootsrap/login_process.php
@@ -1,7 +1,7 @@
 <?php
 $email = $_POST['email'];
 $password = $_POST['password'];
-$email_saya = "arvitaagusk88@gmail.com";
+$email_saya = "afrizal31@gmail.com";
 $password_saya = 12345678;
 if((strcasecmp($email_saya, $email) == 0)&&($password_saya==$password)){
     header("location:index.php?pesan=berhasil");}
