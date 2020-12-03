73c37eae29f4b04732c14ca09f0b9ca7de804df5
AfrizalSA <71804747+AfrizalSA@users.noreply.github.com>
2020-12-03 08:33:04 +0100

    web_bootstrap

--- E41192160_SofyanArifAfRizal/web_bootsrap/login_process.php
+++ E41192160_SofyanArifAfRizal/web_bootsrap/login_process.php
@@ -1,10 +1,17 @@
 <?php
-$email = $_POST['email'];
-$password = $_POST['password'];
-$email_saya = "afrizal31@gmail.com";
-$password_saya = 12345678;
-if((strcasecmp($email_saya, $email) == 0)&&($password_saya==$password)){
-    header("location:index.php?pesan=berhasil");}
-else{
-    header("location:login.php?pesan=gagal");}
+    session_start();
+    include 'koneksi.php';
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $querySql = mysqli_query($koneksi,"SELECT * FROM tb_user Where email='$email'");
+    $data = mysqli_fetch_array($querySql);
+    if ($email == $data['email']) {
+        if($password == $data['password']){
+            header("location:index.php?pesan=berhasil");
+        }else {
+            header("location:login.php?pesan=passwordsalah");
+        }
+    }else{
+        header("location:login.php?pesan=emailsalah");
+    }
     ?>
