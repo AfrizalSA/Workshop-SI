7002d07b8b5421cf068131d7b9865dabdad490c8
AfrizalSA <71804747+AfrizalSA@users.noreply.github.com>
2021-01-27 18:35:37 +0100

    Update db_toko.sql

--- Kasir-Afterkids/db_toko.sql
+++ Kasir-Afterkids/db_toko.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 27, 2021 at 04:13 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- Host: localhost:3306
+-- Erstellungszeit: 28. Jan 2021 um 00:31
+-- Server-Version: 10.3.27-MariaDB
+-- PHP-Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,70 +19,69 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_toko`
+-- Datenbank: `u1011496_db_toko`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang`
+-- Tabellenstruktur für Tabelle `barang`
 --
 
 CREATE TABLE `barang` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `id_barang` varchar(255) NOT NULL,
   `id_kategori` int(11) NOT NULL,
   `nama_barang` text NOT NULL,
   `merk` varchar(255) NOT NULL,
   `warna` varchar(30) NOT NULL,
   `ukuran` varchar(30) NOT NULL,
-  `harga_beli` int(255) UNSIGNED NOT NULL,
-  `harga_jual` int(255) UNSIGNED NOT NULL,
+  `harga_beli` varchar(255) NOT NULL,
+  `harga_jual` varchar(255) NOT NULL,
   `satuan_barang` varchar(255) NOT NULL,
-  `stok` int(100) UNSIGNED NOT NULL,
+  `stok` text NOT NULL,
   `tgl_input` varchar(255) NOT NULL,
   `tgl_update` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `barang`
+-- Daten für Tabelle `barang`
 --
 
 INSERT INTO `barang` (`id`, `id_barang`, `id_kategori`, `nama_barang`, `merk`, `warna`, `ukuran`, `harga_beli`, `harga_jual`, `satuan_barang`, `stok`, `tgl_input`, `tgl_update`) VALUES
-(1, 'BR001', 1, 'T-Shirt Lengan Panjang', 'Afterkids', '', '', 25000, 30000, 'PCS', 10, '6 October 2020, 0:41', '9 January 2021, 7:42'),
-(2, 'BR002', 2, 'Jeans', 'Afterkids', '', '', 50000, 70000, 'PCS', 8, '6 October 2020, 0:41', '9 January 2021, 7:41'),
-(3, 'BR003', 1, 'T-Shirt Polos', 'Afterkids', '', '', 15000, 30000, 'PCS', 8, '6 October 2020, 1:34', '9 January 2021, 7:41'),
-(4, 'BR004', 3, 'Bomber', 'Afterkids', '', '', 90000, 110000, 'PCS', 12, '9 January 2021, 7:40', NULL),
-(5, 'BR005', 4, 'Straw Hat white stripe', 'Adidas', '', '', 25000, 50000, 'PCS', 100, '13 January 2021, 13:09', NULL),
-(8, 'BR007', 2, 'celana panjang hitam', 'Adidas', '', '', 50000, 100000, 'PCS', 67, '13 January 2021, 13:42', NULL),
-(11, 'BR009', 2, 'Celana jeans', 'Levis', '', '', 25000, 50000, 'PCS', 200, '27 January 2021, 20:58', NULL);
+(1, 'BR001', 1, 'T-Shirt Lengan Panjang', 'Afterkids', '', '', '25000', '30000', 'PCS', '20', '6 October 2020, 0:41', '28 January 2021, 0:13'),
+(2, 'BR002', 2, 'Celana Jeans', 'Afterkids', '', '', '50000', '70000', 'PCS', '20', '6 October 2020, 0:41', '28 January 2021, 0:13'),
+(3, 'BR003', 1, 'T-Shirt Polos', 'Afterkids', '', '', '15000', '30000', 'PCS', '20', '6 October 2020, 1:34', '28 January 2021, 0:13'),
+(4, 'BR004', 3, 'Jaket Bomber', 'Afterkids', '', '', '90000', '110000', 'PCS', '20', '9 January 2021, 7:40', '28 January 2021, 0:13'),
+(5, 'BR005', 4, 'Topi Snapback', 'Afterkids', '', '', '70000', '100000', 'PCS', '20', '9 January 2021, 7:42', '28 January 2021, 0:13');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori`
+-- Tabellenstruktur für Tabelle `kategori`
 --
 
 CREATE TABLE `kategori` (
-  `id_kategori` int(11) UNSIGNED NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `nama_kategori` varchar(255) NOT NULL,
   `tgl_input` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `kategori`
+-- Daten für Tabelle `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
-(1, 'Baju', '7 May 2017, 10:23'),
-(2, 'Celana', '7 May 2017, 10:28'),
-(3, 'Jaket', '6 October 2020, 0:19'),
-(4, 'Topi', '6 October 2020, 0:20');
+(1, 'Baju', '27 January 2021, 10:23'),
+(2, 'Celana', '27 January 2021, 10:28'),
+(3, 'Jaket', '27 January 2021, 0:19'),
+(4, 'Topi', '27 January 2021, 0:20'),
+(8, 'Ikat Pinggang', '27 January 2021, 19:24');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Tabellenstruktur für Tabelle `login`
 --
 
 CREATE TABLE `login` (
@@ -92,7 +92,7 @@ CREATE TABLE `login` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `login`
+-- Daten für Tabelle `login`
 --
 
 INSERT INTO `login` (`id_login`, `user`, `pass`, `id_member`) VALUES
@@ -101,7 +101,7 @@ INSERT INTO `login` (`id_login`, `user`, `pass`, `id_member`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `member`
+-- Tabellenstruktur für Tabelle `member`
 --
 
 CREATE TABLE `member` (
@@ -115,16 +115,16 @@ CREATE TABLE `member` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `member`
+-- Daten für Tabelle `member`
 --
 
 INSERT INTO `member` (`id_member`, `nm_member`, `alamat_member`, `telepon`, `email`, `gambar`, `NIK`) VALUES
-(1, 'Aditya Yoga Pamungkas', 'Jl. Tegalbesar - Jember', '082235301951', 'adityapamungkas@gmail.com', 'Logo.png', '12345678');
+(1, 'Aditya Yoga', 'Jl. Tegalbesar - Jember', '082235301951', 'adityapamungkas@gmail.com', 'logo afterkids.png', '12345678');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nota`
+-- Tabellenstruktur für Tabelle `nota`
 --
 
 CREATE TABLE `nota` (
@@ -138,54 +138,35 @@ CREATE TABLE `nota` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `nota`
+-- Daten für Tabelle `nota`
 --
 
 INSERT INTO `nota` (`id_nota`, `id_barang`, `id_member`, `jumlah`, `total`, `tanggal_input`, `periode`) VALUES
-(1, 'BR001', 1, '4', '12000', '6 October 2020, 0:45', '10-2020'),
-(2, 'BR001', 1, '4', '12000', '6 October 2020, 0:45', '10-2020'),
-(3, 'BR001', 1, '4', '12000', '6 October 2020, 0:45', '10-2020'),
-(4, 'BR001', 1, '4', '12000', '6 October 2020, 0:45', '10-2020'),
-(5, 'BR001', 1, '2', '6000', '6 October 2020, 0:49', '10-2020'),
-(6, 'BR001', 1, '2', '6000', '6 October 2020, 0:49', '10-2020'),
-(7, 'BR001', 1, '2', '6000', '6 October 2020, 1:15', '10-2020'),
-(8, 'BR002', 1, '2', '6000', '6 October 2020, 1:17', '10-2020'),
-(9, 'BR001', 1, '2', '6000', '6 October 2020, 1:20', '10-2020'),
 (10, 'BR001', 1, '1', '30000', '9 January 2021, 2:39', '01-2021'),
 (11, 'BR002', 1, '2', '140000', '9 January 2021, 2:39', '01-2021'),
 (12, 'BR003', 1, '2', '60000', '9 January 2021, 2:39', '01-2021'),
-(13, 'BR001', 1, '1', '30000', '9 January 2021, 2:39', '01-2021'),
-(14, 'BR002', 1, '2', '140000', '9 January 2021, 2:39', '01-2021'),
-(15, 'BR003', 1, '2', '60000', '9 January 2021, 2:39', '01-2021'),
-(16, 'BR001', 1, '1', '30000', '9 January 2021, 2:39', '01-2021'),
-(17, 'BR002', 1, '2', '140000', '9 January 2021, 2:39', '01-2021'),
-(18, 'BR003', 1, '2', '60000', '9 January 2021, 2:39', '01-2021'),
-(19, 'BR001', 1, '0', '0', '13 January 2021, 21:45', '01-2021'),
-(20, 'BR001', 1, '0', '0', '13 January 2021, 21:45', '01-2021'),
-(21, 'BR001', 1, '0', '0', '13 January 2021, 21:48', '01-2021'),
-(22, 'BR001', 1, '0', '0', '13 January 2021, 21:48', '01-2021'),
-(23, 'BR001', 1, '0', '0', '13 January 2021, 21:48', '01-2021'),
-(24, 'BR001', 1, '0', '0', '13 January 2021, 22:37', '01-2021'),
-(25, 'BR007', 1, '50', '5000000', '27 January 2021, 10:52', '01-2021'),
-(26, 'BR007', 1, '1', '100000', '27 January 2021, 21:08', '01-2021'),
-(27, 'BR007', 1, '1', '100000', '27 January 2021, 21:42', '01-2021'),
-(28, 'BR007', 1, '1', '100000', '27 January 2021, 21:42', '01-2021'),
-(29, 'BR007', 1, '2', '200000', '27 January 2021, 21:42', '01-2021'),
-(30, 'BR007', 1, '2', '200000', '27 January 2021, 21:42', '01-2021'),
-(31, 'BR007', 1, '2', '200000', '27 January 2021, 21:42', '01-2021'),
-(32, 'BR007', 1, '5', '500000', '27 January 2021, 21:57', '01-2021'),
-(33, 'BR007', 1, '5', '500000', '27 January 2021, 21:57', '01-2021'),
-(34, 'BR007', 1, '5', '500000', '27 January 2021, 21:57', '01-2021'),
-(35, 'BR007', 1, '10', '1000000', '27 January 2021, 21:57', '01-2021');
+(13, 'BR005', 1, '3', '300000', '27 January 2021, 17:10', '01-2021'),
+(14, 'BR004', 1, '3', '330000', '27 January 2021, 19:24', '01-2021'),
+(15, 'BR005', 1, '1', '100000', '27 January 2021, 19:24', '01-2021'),
+(16, 'BR002', 1, '4', '280000', '27 January 2021, 19:25', '01-2021'),
+(17, 'BR001', 1, '2', '60000', '27 January 2021, 20:06', '01-2021'),
+(18, 'BR003', 1, '3', '90000', '27 January 2021, 20:07', '01-2021'),
+(19, 'BR004', 1, '1', '110000', '27 January 2021, 20:07', '01-2021'),
+(20, 'BR001', 1, '2', '60000', '27 January 2021, 20:06', '01-2021'),
+(21, 'BR003', 1, '3', '90000', '27 January 2021, 20:07', '01-2021'),
+(22, 'BR004', 1, '1', '110000', '27 January 2021, 20:07', '01-2021'),
+(23, 'BR001', 1, '2', '60000', '27 January 2021, 20:06', '01-2021'),
+(24, 'BR003', 1, '3', '90000', '27 January 2021, 20:07', '01-2021'),
+(25, 'BR004', 1, '1', '110000', '27 January 2021, 20:07', '01-2021');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `penjualan`
+-- Tabellenstruktur für Tabelle `penjualan`
 --
 
 CREATE TABLE `penjualan` (
-  `id_penjualan` int(11) UNSIGNED NOT NULL,
+  `id_penjualan` int(11) NOT NULL,
   `id_barang` varchar(255) NOT NULL,
   `id_member` int(11) NOT NULL,
   `jumlah` varchar(255) NOT NULL,
@@ -196,7 +177,7 @@ CREATE TABLE `penjualan` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `toko`
+-- Tabellenstruktur für Tabelle `toko`
 --
 
 CREATE TABLE `toko` (
@@ -208,100 +189,100 @@ CREATE TABLE `toko` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `toko`
+-- Daten für Tabelle `toko`
 --
 
 INSERT INTO `toko` (`id_toko`, `nama_toko`, `alamat_toko`, `tlp`, `nama_pemilik`) VALUES
-(1, 'Afterkids', 'Jl. Tegalbesar - Jember', '082235301951', 'Aditya Yoga Pratama');
+(1, 'Afterkids', 'Jl. Tegal Besar - Jember', '082235301951', 'Aditya Yoga Pamungkas');
 
 --
--- Indexes for dumped tables
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `barang`
+-- Indizes für die Tabelle `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `kategori`
+-- Indizes für die Tabelle `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indexes for table `login`
+-- Indizes für die Tabelle `login`
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id_login`);
 
 --
--- Indexes for table `member`
+-- Indizes für die Tabelle `member`
 --
 ALTER TABLE `member`
   ADD PRIMARY KEY (`id_member`);
 
 --
--- Indexes for table `nota`
+-- Indizes für die Tabelle `nota`
 --
 ALTER TABLE `nota`
   ADD PRIMARY KEY (`id_nota`);
 
 --
--- Indexes for table `penjualan`
+-- Indizes für die Tabelle `penjualan`
 --
 ALTER TABLE `penjualan`
   ADD PRIMARY KEY (`id_penjualan`);
 
 --
--- Indexes for table `toko`
+-- Indizes für die Tabelle `toko`
 --
 ALTER TABLE `toko`
   ADD PRIMARY KEY (`id_toko`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT for table `barang`
+-- AUTO_INCREMENT für Tabelle `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `kategori`
+-- AUTO_INCREMENT für Tabelle `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `login`
+-- AUTO_INCREMENT für Tabelle `login`
 --
 ALTER TABLE `login`
   MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `member`
+-- AUTO_INCREMENT für Tabelle `member`
 --
 ALTER TABLE `member`
   MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `nota`
+-- AUTO_INCREMENT für Tabelle `nota`
 --
 ALTER TABLE `nota`
-  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
--- AUTO_INCREMENT for table `penjualan`
+-- AUTO_INCREMENT für Tabelle `penjualan`
 --
 ALTER TABLE `penjualan`
-  MODIFY `id_penjualan` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
--- AUTO_INCREMENT for table `toko`
+-- AUTO_INCREMENT für Tabelle `toko`
 --
 ALTER TABLE `toko`
   MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
